3d480ca9a1d44120af70d3c0a227dc9d1e78ea00
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-07-15 04:19:36 +0300

    commit tree 3c3e29333a4eb63ddd1910179080e6319c2e5fdf

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const arrList = links.map((link) =>(
+    <a key={link} href={"#" +link}>{link}</a>
+    
+  ))
+
+  return <nav>{arrList}</nav>;
 }
 
 export default NavBar;
